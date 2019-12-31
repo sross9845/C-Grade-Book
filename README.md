d73445b955ff631d64c5392d9b27eb27c2e2786f
Sean Ross <sean.ross.985@gmail.com>
2019-12-30 22:34:17 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+"# Csharp-Grade-Book"  git init git add -A git commit -m "first commit" git remote add origin https://github.com/sross9845/Csharp-Grade-Book.git git push -u origin master
+"# Csharp-Grade-Book"  git init git add -A git commit -m "first commit" git remote add origin https://github.com/sross9845/Csharp-Grade-Book.git git push -u origin master
+# Csharp-Grade-Book git init git add -A git commit -m first commit git remote add origin https://github.com/sross9845/Csharp-Grade-Book.git git push -u origin master
---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    public class Book
+    {
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach( var number in grades)
+            {
+                lowGrade = Math.Min(number, lowGrade);
+                highGrade = Math.Max(number, highGrade);
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The Lowest grade is {lowGrade}");
+            Console.WriteLine($"The highest grade is {highGrade}");
+            Console.WriteLine($"The average grade is {result:N1}");
+        }
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+         public List<double> grades;
+        public string name;
+    }
+}
